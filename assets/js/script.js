2366f00d568e5ca9d278ed2fcbd559c84708b592
skouch <skouchy24@gmail.com>
2023-02-01 01:51:29 -0700

    feat(header.js): linked current date and time to <header> in .js. Time and date formatted to be reader friendly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,61 @@
+let taskEl = document.querySelector('.time-block');
+let buttonEl = document.querySelector('.saveBtn');
+
+let today = dayjs().format('MMM D, YYYY');
+$('#currentDay').text(today);
+let currentTime;
+let time;
+
+// minutes:seconds formatted in 2-digit display
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i
+  }
+  return i;
+}
+
+// getDate data : hours:minutes:seconds
+function clock() {
+  time = new Date();
+  return {
+    hours: time.getHours(),
+    minutes: addZero(time.getMinutes()),
+    seconds: addZero(time.getSeconds()),
+  }
+}
+
+// displays time in 12hr format
+function formatHoursTo12() {
+  return time.hours % 12 || 12;
+}
+
+// begins clock interval onload to display current formatted time
+setInterval(function () {
+  time = clock();
+  let formatHour = formatHoursTo12();
+  currentTime = (formatHour + ':' + time.minutes);
+  let ampm = (formatHour <= 12) ? [currentTime + "am"] : [currentTime+"pm"]; 
+  $('#currentTime').text(ampm);
+  timeBlockColor();
+}, 1000);
+
+function timeBlockColor() {
+  // if ()
+  console.log('currentTime in timeBlock func');
+  console.log(currentTime);
+}
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
+// $(function (event) {
+  // TODO: Add a listener for click events on the save button. This code should
+  // taskItemEl.addEventListener('submit', value);
+  // *use the id in the containing time-block as a key to save the user input in
+    // *local storage. HINT: What does `this` reference in the click listener
+    // *function? How can DOM traversal be used to get the "hour-x" id of the
+    // *time-block containing the button that was clicked? How might the id be
+    // *useful when saving the description in local storage?
     //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
@@ -20,4 +68,4 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+  // });
